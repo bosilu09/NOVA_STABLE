5207ca511e84e35db0a86a3684657aa20eb11759
bosilu09 <84651759+bosilu09@users.noreply.github.com>
2023-05-12 15:05:06 +0530

    Test

--- Commands/Media/play.js
+++ Commands/Media/play.js
@@ -22,7 +22,7 @@ module.exports = {
       const song = songInfo.videos[0];
       let videoUrl = song.url;
       let videoId = videoUrl.split("v=")[1];
-Miku.sendMessage(m.from,{text:`_${song.title}_`})
+Miku.sendMessage(m.from,{text:`_${song.duration}_`})
       await Miku.sendMessage(
         m.from,
         {
