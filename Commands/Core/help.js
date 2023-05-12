1e07ec9d77f10985d370ed8d9fce6dc55f120c7a
bosilu09 <84651759+bosilu09@users.noreply.github.com>
2023-05-12 14:23:23 +0530

    MAJOR

--- Commands/Core/help.js
+++ Commands/Core/help.js
@@ -1,9 +1,8 @@
-const Botversion = require("./hello.js")
-  module.exports = {
-  name: "menu",
-  alias: ["help", "h", "helpm", "helpmenu","list"],
+module.exports = {
+  name: "help",
+  alias: ["menu", "h", "helpm", "helpmenu"],
   desc: "Gives all bot commands list",
-  react: "🗃️",
+  react: "✨",
   category: "Core",
   start: async (
     Miku,
@@ -19,9 +18,9 @@ const Botversion = require("./hello.js")
       if (!cmd || cmd.type == "hide") return m.reply("No Command Found");
       else
         data.push(
-          `🥷 Command : ${cmd.name.replace(/^\w/, (c) => c.toUpperCase())}`
+          `🥷Command : ${cmd.name.replace(/^\w/, (c) => c.toUpperCase())}`
         );
-      if (cmd.alias) data.push(`👾Alias : ${cmd.alias.join(", ")}`);
+      if (cmd.alias) data.push(`🌐Alias : ${cmd.alias.join(", ")}`);
       if (cmd.cool) data.push(`⏱️Cooldown: ${cmd.cool}`);
       if (cmd.desc) data.push(`🧾Description : ${cmd.desc}`);
       if (cmd.usage)
@@ -34,13 +33,13 @@ const Botversion = require("./hello.js")
       var buttonss = [
         {
           buttonId: `${prefix}help`,
-          buttonText: { displayText: `Menu` },
+          buttonText: { displayText: `help` },
           type: 1,
         },
       ];
       let buth = {
         text: `ℹ️Command Info\n\n${data.join("\n")}`,
-        footer: `Inovatech Bot`,
+        footer: `${botName}`,
         buttons: buttonss,
         headerType: 1,
       };
@@ -55,18 +54,27 @@ const Botversion = require("./hello.js")
         };
         const uptime = () => formatTime(process.uptime());
 
-      let textHelpMenu = `Hi *${pushName}* ,
+const now = new Date();
+        const hour = now.getHours();
+       let greeting;
 
-I am *NOVA BOT*🥷, a bot developed by *𝐓𝐞𝐚𝐦 𝐍𝐎𝐕𝐀*.
+        if (hour >= 0 && hour < 12) {
+          greeting = "Good Morning 🌌"; //good morning
+        } else if (hour >= 12 && hour < 18) {
+          greeting = "Good Afternoon 🔆"; //good afternoon
+        } else {
+          greeting = "Good Evening ✨"; //good evening
+        }
+      let textHelpMenu = `${greeting} *${pushName}*,
 
-🥷 Version :- _1.3.5 Stable_
-      
-💫 My prefix is: *${prefix}*
-      
+I am *${botName}*, a bot developed by *Team NOVA*.
+
+🥷 My prefix is: *${prefix}*
+
 🧩 Server Uptime: *${uptime()}*
-  
+
 Here's the list of my Commands.\n
-                 
+           
 ╭──────ꕥ Core ꕥ─────╮
 ├
 ├・🎐 ʜɪ, ʜᴇʟᴘ, 
@@ -223,8 +231,9 @@ Here's the list of my Commands.\n
 ├
 ╰──────────────────╯\n\n`;
 
-      textHelpMenu += `*🔰  ${botName}  🔰*
- _Powered By:_ *Team ATLAS*
+      
+      textHelpMenu += `*🥷  ${botName}  🥷*
+ _Powered By:_ *Team NOVA*
 
  🥷 To use any of these commands type " *${prefix}Command name* ".
 
@@ -232,177 +241,7 @@ Here's the list of my Commands.\n
 
 🧩 To report any issues to Developer type " *${prefix}report <describe issue>* ".\n`;
 
-      await Miku.sendMessage(
-        m.from,
-        {
-          image:{url:"https://graph.org/file/27054bdb82718a29de80c.png"},
-          caption:` Hi *${pushName}* ,
-
-          I am *NOVA BOT*🥷, a bot developed by *𝐓𝐞𝐚𝐦 𝐍𝐎𝐕𝐀*.
-          
-          🥷 Version :- _1.3.5 Stable_
-                
-          💫 My prefix is: *${prefix}*
-                
-          🧩 Server Uptime: *${uptime()}*
-            
-          Here's the list of my Commands.\n
-                           
-          ╭──────ꕥ Core ꕥ─────╮
-          ├
-          ├・🎐 ʜɪ, ʜᴇʟᴘ, 
-          ├・🎐 ᴄᴏᴜᴘʟᴇᴘᴘ, ᴏᴡɴᴇʀ, 
-          ├・🎐 ꜱᴄʀɪᴘᴛ, ꜱᴛᴀʟᴋ, 
-          ├・🎐 ꜱᴜᴘᴘᴏʀᴛ, ʀᴀɴᴋ
-          ├
-          ╰──────────────────╯
-          ╭─────ꕥ Group ꕥ─────╮
-          ├
-          ├・🏮 ᴀᴅᴍɪɴꜱ, ᴀɴɴᴏᴜɴᴄᴇ,
-          ├・🏮 ᴀɴᴛɪʟɪɴᴋɢᴄ, ʙᴏᴛꜱᴡɪᴛᴄʜ, 
-          ├・🏮 ᴄʜᴀɴɢᴇɢᴄɴᴀᴍᴇ, ᴄʜᴀᴛʙᴏᴛɢᴄ, 
-          ├・🏮 ᴅᴇʟᴇᴛᴇ, ᴘʀᴏᴍᴏᴛᴇ, 
-          ├・🏮 ᴅᴇᴍᴏᴛᴇ, ɢᴄʟɪɴᴋ, 
-          ├・🏮 ɢʀᴏᴜᴘ, ɢʀᴏᴜᴘɪɴꜰᴏ, 
-          ├・🏮 ʜɪᴅᴇᴛᴀɢ, ʟᴇᴀᴠᴇ, 
-          ├・🏮 ɴꜱꜰᴡ, ʀᴇᴍᴏᴠᴇ, 
-          ├・🏮 ʀᴇᴘᴏʀᴛ, ʀᴇᴠᴏᴋᴇ, 
-          ├・🏮 ꜱᴇᴛɢᴄᴅᴇꜱᴄ, ꜱᴇᴛᴘᴘɢᴄ, 
-          ├・🏮 ᴛᴀɢᴀʟʟ, ᴡᴇʟᴄᴏᴍᴇ
-          ├
-          ╰──────────────────╯
-          ╭──────ꕥ Mods ꕥ──────╮
-          ├
-          ├・🥷 ᴀᴅᴅᴍᴏᴅ, ᴅᴇʟᴇᴛᴇᴍᴏᴅ, 
-          ├・🥷 ʙᴀɴ, ᴜɴʙᴀɴ, 
-          ├・🥷 ʙᴀɴɢᴄ, ᴜɴʙᴀɴɢᴄ, 
-          ├・🥷 ʙʟᴏᴄᴋ, ᴜɴʙʟᴏᴄᴋ, 
-          ├・🥷 ʙʀᴏᴀᴅᴄᴀꜱᴛ, ᴄʜᴀʀʟɪꜱᴛ, 
-          ├・🥷 ᴍᴏᴅᴇ, ʙᴀɴʟɪꜱᴛ, 
-          ├・🥷 ᴘᴍᴄʜᴀᴛʙᴏᴛ, ᴄʜᴀʀʟɪꜱᴛ, 
-          ├・🥷 ꜱᴇᴛᴄʜᴀʀᴀᴄᴛᴇʀ
-          ├
-          ╰──────────────────╯          
-          ╭──────ꕥ Media ꕥ─────╮
-          ├
-          ├・💫 ɪɢᴅʟ,  
-          ├・💫 ꜱᴏɴɢ <ꜱᴏɴɢ ɴᴀᴍᴇ>, 
-          ├・💫 ᴠɪᴅᴇᴏ <ᴠɪᴅᴇᴏ ɴᴀᴍᴇ>, 
-          ├・💫 ʏᴛᴍᴘ3 <ʟɪɴᴋ>, 
-          ├・💫 ʏᴛᴍᴘ4 <ʟɪɴᴋ>, 
-          ├・💫 ʏᴛᴅᴏᴄ <ʟɪɴᴋ>
-          ├・💫 ᴘʟᴀʏʟɪꜱᴛ, ʏᴛꜱ,
-          ├・💫 ᴛɪᴋᴛᴏᴋᴀᴜᴅɪᴏ <ʟɪɴᴋ>,
-          ├・💫 ᴛɪᴋᴛᴏᴋᴠɪᴅᴇᴏ <ʟɪɴᴋ>, 
-          ├・💫 ᴛɪᴄᴛᴏᴋᴅᴏᴄ <ʟɪɴᴋ>,
-          ├
-          ╰──────────────────╯
-          ╭─────ꕥ Search ꕥ─────╮
-          ├
-          ├・🔎 ᴀɴɪᴍᴇ, ɢɪꜰꜱᴇᴀʀᴄʜ, 
-          ├・🔎 ɢɪᴍᴀɢᴇ, ᴘɪɴᴛᴇʀᴇꜱᴛ, 
-          ├・🔎 ɢɪᴛʜᴜʙ, ɢᴏᴏɢʟᴇ, 
-          ├・🔎 ʟʏʀɪᴄꜱ, ʀɪɴɢᴛᴏɴᴇ, 
-          ├・🔎 ꜱᴛɪᴄᴋᴇʀꜱᴇᴀʀᴄʜ, ᴡᴇᴀᴛʜᴇʀ, 
-          ├・🔎 ʏᴏᴜᴛᴜʙᴇꜱᴇᴀʀᴄʜ
-          ├
-          ╰──────────────────╯
-          ╭────ꕥ Utilities ꕥ────╮
-          ├
-          ├・🎗 ᴇᴍᴏᴊɪᴍɪx, Q / Qᴜᴏᴛᴇ, 
-          ├・🎗 ꜱᴛɪᴄᴋᴇʀ, ꜱᴛɪᴄᴋᴇʀᴄʀᴏᴘ, 
-          ├・🎗 ꜱᴛᴇᴀʟ, ᴛᴏᴀᴜᴅɪᴏ, 
-          ├・🎗 ᴛᴏᴍᴘ3, ᴛᴏᴍᴘ4, 
-          ├・🎗 ᴛᴏᴜʀʟ, ꜱᴛɪᴄᴋᴇʀᴍᴇᴍᴇ,
-          ├・🎗 ᴛᴇxᴛᴅᴇꜱɪɢɴ
-          ├
-          ╰──────────────────╯
-          ╭────ꕥ Image Edit ꕥ────╮
-          ├
-          ├・💠 ʙʟᴜʀ, ᴄɪʀᴄʟᴇ, 
-          ├・💠 ᴊᴀɪʟ, ʀᴇᴍᴏᴠᴇʙɢ, 
-          ├・💠 ᴛʀɪɢɢᴇʀ
-          ├
-          ╰──────────────────╯
-          ╭────ꕥ Audio Edit ꕥ────╮
-          ├
-          ├・🎧 ʙᴀꜱꜱ, ʙʟᴏᴡɴ, 
-          ├・🎧 ᴅᴇᴇᴘ, ꜰᴀᴛ, 
-          ├・🎧 ɴɪɢʜᴛᴄᴏʀᴇ, ʀᴇᴠᴇʀꜱᴇ, 
-          ├・🎧 ʀᴏʙᴏᴛ, ꜱʟᴏᴡ, 
-          ├・🎧 ꜱᴍᴏᴏᴛʜ, ᴛᴇᴍᴘᴏ
-          ╰──────────────────╯
-          ╭────ꕥ Essentials ꕥ────╮
-          ├
-          ├・🧩 ᴇʟᴇᴍᴇɴᴛ, ꜱᴄʀᴇᴇɴꜱʜᴏᴛ
-          ├・🧩 Qᴜᴇꜱᴛɪᴏɴ, ꜱᴀʏ, 
-          ├・🧩 ꜱᴀʏᴊᴀᴘᴀɴᴇꜱᴇ, ꜱᴀʏʙᴇɴɢᴀʟɪ, 
-          ├・🧩 ꜱᴀʏʜɪɴᴅɪ, ᴜᴅɪᴄᴛɪᴏɴᴀʀʏ
-          ├
-          ╰───────────────────────╯
-          ╭────ꕥ Weeb ꕥ────╮
-          ├
-          ├・ 🧧 ᴀɴɪᴍᴇQᴜᴏᴛᴇ, ᴄᴏꜱᴘʟᴀʏ, 
-          ├・ 🧧 ᴄᴏꜱᴘʟᴀʏᴠɪᴅᴇᴏ, ꜰᴏxɢɪʀʟ, 
-          ├・ 🧧 ᴍᴀɪᴅ, ᴡᴀʟʟᴘᴀᴘᴇʀ, 
-          ├・ 🧧 ᴡᴀɪꜰᴜ
-          ├
-          ╰──────────────────╯
-          ╭────ꕥ Reactions ꕥ────╮
-          ├
-          ├・ 🎋 ʙᴇʜᴀᴘᴘʏ, ʙɪᴛᴇ, 
-          ├・ 🎋 ʙᴏɴᴋ, ʙᴜʟʟʏ, 
-          ├・ 🎋 ᴄʀʏ, ᴅᴀɴᴄᴇ, 
-          ├・ 🎋 ʜᴀɴᴅʜᴏɴᴅ, ʜᴀᴘᴘʏ, 
-          ├・ 🎋 ʜɪɢʜꜰɪᴠᴇ, ʜᴜɢ, 
-          ├・ 🎋 ᴋɪᴄᴋ, ᴋɪʟʟ, 
-          ├・ 🎋 ᴋɪꜱꜱ, ᴘᴀᴛ, 
-          ├・ 🎋 ꜱʟᴀᴘ, ꜱᴍɪʟᴇ, 
-          ├・ 🎋 ᴡᴀᴠᴇ, ᴡɪɴᴋ, 
-          ├・ 🎋 ʏᴇᴇᴛ
-          ├
-          ╰──────────────────╯
-          ╭────ꕥ Logo Maker ꕥ────╮
-          ├
-          ├・ 🎄 3ᴅᴄʜʀɪꜱᴛᴍᴀꜱ, 3ᴅɴᴇᴏɴ, 
-          ├・ 🎄 3ᴅꜱᴘᴀᴄᴇ, 3ᴅꜱᴛᴏɴᴇ,
-          ├・ 🎄 ʙᴇᴀʀ, ʙʟᴀᴄᴋᴘɪɴᴋ,
-          ├・ 🎄 ʙʟᴏᴏᴅ, ʙᴏᴋᴇʜ, 
-          ├・ 🎄 ᴄᴀɴᴅʏ, ᴄᴀʀʙᴏɴ, 
-          ├・ 🎄 ᴄʜᴏᴄᴏʟᴀᴛᴇ, ᴄʜʀɪꜱᴛᴍᴀꜱ, 
-          ├・ 🎄 ᴄɪʀᴄᴜɪᴛ, ᴄʟᴏᴜᴅ, 
-          ├・ 🎄 ᴅᴇᴇᴘꜱᴇᴀ, ᴅᴇᴍᴏɴ, 
-          ├・ 🎄 ᴅʀᴏᴘᴡᴀᴛᴇʀ, ɢʟɪᴛᴄʜ, 
-          ├・ 🎄 ɢʟɪᴛᴄʜ2, ɢʟɪᴛᴄʜ3 
-          ├・ 🎄 ɢʀᴀꜰꜰɪᴛɪ, ʜᴏʟʟᴏɢʀᴀᴘʜɪᴄ, 
-          ├・ 🎄 ᴊᴏᴋᴇʀ, ʟɪᴏɴ , ᴍᴀɢᴍᴀ, 
-          ├・ 🎄 ᴍᴀᴛʀɪx, ɴᴇᴏɴ, 
-          ├・ 🎄 ɴᴇᴏɴᴅᴇᴠɪʟ, ɴᴇᴏɴɢʀᴇᴇɴ, 
-          ├・ 🎄 ɴᴇᴏɴʟɪɢʜᴛ, ᴘᴀᴘᴇʀᴄᴜᴛ, 
-          ├・ 🎄 ᴘᴇɴᴄɪʟ, ᴘᴏʀɴʜᴜʙ, 
-          ├・ 🎄 ꜱᴄɪꜰɪ, ꜱᴘᴀʀᴋʟᴇᴄʜʀɪꜱᴛᴍᴀꜱ, 
-          ├・ 🎄 ᴛʜᴜɴᴅᴇʀ, ᴛʜᴜɴᴅᴇʀ2, 
-          ├・ 🎄 ᴛʀᴀɴꜱꜰᴏʀᴍᴇʀ, ᴡᴀʟʟ,
-          ├・ 🎄 ᴡᴏʟꜰ
-          ├
-          ╰──────────────────╯
-          ╭────ꕥ Mine Craft ꕥ────╮
-          ├
-          ├・ 🎒 ʙᴜʏ, ɪɴᴠᴇɴᴛᴏʀʏ, 
-          ├・ 🎒 ᴍɪɴᴇ, ꜱʜᴏᴘ, 
-          ├・ 🎒 ʀᴇɢ-ɪɴᴠ, 
-          ├
-          ╰──────────────────╯
-          ╭────ꕥ ECONOMY ꕥ────╮
-          ├
-          ├・ 🔖 ʙᴀɴᴋ, ᴄᴀᴘᴀᴄɪᴛʏ, 
-          ├・ 🔖 ᴅᴀɪʟʏ, ᴅᴇᴘᴏꜱɪᴛ, 
-          ├・ 🔖 ɢᴀᴍʙʟᴇ, ʟʙ, 
-          ├・ 🔖 ʀᴏʙ, ꜱʟᴏᴛ, 
-          ├・ 🔖 ᴛʀᴀɴꜱꜰᴇʀ, ᴡᴀʟʟᴇᴛ, 
-          ├・ 🔖 ᴡɪᴛʜᴅʀᴀᴡ
-          ├
-          ╰──────────────────╯`
-        });
+      await Miku.sendMessage(m.from, {image: { url: "https://graph.org/file/27054bdb82718a29de80c.png" }, caption: textHelpMenu,}, { quoted: m });
+    }
   },
 };
