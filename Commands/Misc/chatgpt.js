2f8806ff2c50869f20d13f5b693147d09bbe58d3
bosilu09 <84651759+bosilu09@users.noreply.github.com>
2023-06-03 23:47:46 +0530

    CHATGPT ADDED

---
+++ Commands/Misc/chatgpt.js
@@ -0,0 +1,31 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({apiKey: "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",});
+
+
+module.exports = {
+    name: "chatgpt",
+    alias: ["ytplay", "ytv"],
+    desc: "To play a song from youtube",
+    category: "Media",
+    usage: `play <song name>`,
+    react: "🖥",
+    start: async (Miku, m, { text, prefix, args }) => {
+      if (!args[0]){
+        m.reply("⚖ _Your Prompt is Empty Please Add one Before Trying Again_\n\nexample: .chatgpt hi how are you")
+      return;}
+const openai = new OpenAIApi(configuration);
+
+async function chatthr(prompt){
+console.log(`User : ${args.join(" ")}`)
+const completion = await openai.createChatCompletion({
+  model: "gpt-3.5-turbo",
+  messages: [{role: "user", content: `${prompt}`}],
+});
+console.log(`ChatGPT : ${completion.data.choices[0].message.content}`);
+
+m.reply(`👤 *User* : _${args.join(" ")}_\n\n🖥 *ChatGPT* : _${completion.data.choices[0].message.content}_`)
+}
+chatthr(args.join(" "))
+    }
+}
